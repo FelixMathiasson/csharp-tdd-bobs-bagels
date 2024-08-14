02f2154d4a1fc4e214fa9485dcb1430806c37f26
Felix <felix.mathiasson99@gmail.com>
2024-08-14 13:47:38 +0200

    wrote test case for adding

--- csharp-tdd-bobs-bagels.tests/BasketTests.cs
+++ csharp-tdd-bobs-bagels.tests/BasketTests.cs
@@ -1,20 +1,34 @@
+using tdd_bobs_bagels.CSharp.Main;
 namespace csharp_tdd_bobs_bagels.tests;
 
 public class BasketTests
 {
-    //[SetUp]
-    //public void Setup()
-    //{
-    //}
-
     [TestCase("Plain")]
+    [TestCase("Chocolate")]
+    [TestCase("Vanilla")]
+    [TestCase("Strawberry")]
+    [TestCase("Nutella")]
+    [TestCase("Plain")]
+    [TestCase("Vanilla")]
+    [TestCase("Banana")]
     public void TestAddingBagel(string name)
     {
-        
+        Basket basket = new Basket();
+        bool expected = basket.IsBasketFull(); //get get expected value
 
+        bool result = basket.AddBagel(name); //see if bagel can be added
 
-
-
-        Assert.Pass();
+        Assert.That(expected == result);
     }
+
+
+
+
+
+    //[TestCase("Vanilla")]
+    //public void TestRemovingBagle(string name)
+    //{
+    //    Basket basket = new Basket();
+
+    //}
 }
