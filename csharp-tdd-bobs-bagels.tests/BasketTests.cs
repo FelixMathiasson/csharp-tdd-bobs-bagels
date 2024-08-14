da7ed62be8534c1e267491c9a06d17a85d02adee
Felix <felix.mathiasson99@gmail.com>
2024-08-14 13:24:27 +0200

    renamed a few things

---
+++ csharp-tdd-bobs-bagels.tests/BasketTests.cs
@@ -0,0 +1,20 @@
+namespace csharp_tdd_bobs_bagels.tests;
+
+public class BasketTests
+{
+    //[SetUp]
+    //public void Setup()
+    //{
+    //}
+
+    [TestCase("Plain")]
+    public void TestAddingBagel(string name)
+    {
+        
+
+
+
+
+        Assert.Pass();
+    }
+}
