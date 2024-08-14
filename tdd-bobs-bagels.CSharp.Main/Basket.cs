e6a8683031947ae1af456af1b79e9c69ce7b5fce
Felix <felix.mathiasson99@gmail.com>
2024-08-14 13:47:57 +0200

    Added source code for passing add test

--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -8,6 +8,27 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Basket
     {
-        
+        private List<string> orderedBagels = new List<string>();
+        private int basketSize = 5; // 5 just as a starting 
+
+        public bool AddBagel(string name)
+        {
+           if(orderedBagels.Count < basketSize) //still space left
+           {
+                orderedBagels.Add(name);
+                return true;
+           }
+            Console.WriteLine("Could not add bagel to basket! Basket already full!");
+            return false;
+        }
+
+        public bool IsBasketFull()
+        {
+            if(orderedBagels.Count < basketSize) //basket not full
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
